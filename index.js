a618e0a407fbc7c5245773c14bff45ac78718cc9
Daniel Lando <daniel.sorridi@gmail.com>
2022-10-11 17:20:40 +0200

    fix: add badges code

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const badge = require('./lib/badge.js')
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.get('/', (req, res) => {
+    // serve index.html
+    res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/image/:namespace/:image', async (req, res) => {
+    const { namespace, image } = req.params
+    const data = await badge(namespace === '_' ? null : namespace, image)
+    res.set('Content-Type', data.type)
+    res.send(data.body)
+})
+
+app.get('/image/:image', async (req, res) => {
+    const { image } = req.params
+    const data = await badge(null, image)
+    res.set('Content-Type', data.type)
+    res.send(data.body)
+})
+
 app.listen(process.env.PORT || 3000)
